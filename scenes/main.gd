2f9f003f9afe1cde11b7333ee6a03a4f8e28525c
kicerKs <osk.ch18@gmail.com>
2025-06-26 18:54:17 +0200

    audio test 2

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,4 +4,8 @@ func _ready():
 	Lobby.player_loaded.rpc_id(1)
 
 func start_game():
+	play_background_music.rpc()
+
+@rpc("call_local", "any_peer", "reliable")
+func play_background_music():
 	$AudioStreamPlayer.play()
