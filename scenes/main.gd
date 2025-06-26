524774126dc3938f7b1db735b1a08a789cb97025
kicerKs <osk.ch18@gmail.com>
2025-06-26 19:07:02 +0200

    audio machen part 2

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,8 @@ extends Node
 
 func _ready():
 	Lobby.player_loaded.rpc_id(1)
+	SignalBus.player_won.connect(play_victory_music)
+	SignalBus.player_eliminated.connect(play_elimination_music)
 
 func start_game():
 	play_background_music.rpc()
@@ -9,3 +11,15 @@ func start_game():
 @rpc("call_local", "any_peer", "reliable")
 func play_background_music():
 	$AudioStreamPlayer.play()
+
+@rpc("call_local", "any_peer", "reliable")
+func play_victory_music(id):
+	if id == multiplayer.get_unique_id():
+		$WinMusic.play()
+		$AudioStreamPlayer.stop()
+
+@rpc("call_local", "any_peer", "reliable")
+func play_elimination_music(id):
+	if id == multiplayer.get_unique_id():
+		$DefeatMusic.play()
+		$AudioStreamPlayer.stop()
