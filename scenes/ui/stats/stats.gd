e1efdede1824d3018635a671862a7c921d4d7ab9
kicerKs <osk.ch18@gmail.com>
2025-07-03 20:09:22 +0200

    fix for damian

--- scenes/ui/stats/stats.gd
+++ scenes/ui/stats/stats.gd
@@ -4,11 +4,12 @@ var button = load("res://scenes/ui/stats/stats_button.tscn")
 
 func _ready() -> void:
 	var dir := DirAccess.open("user://stats")
-	dir.list_dir_begin()
-	for file: String in dir.get_files():
-		var but = button.instantiate()
-		but.setup(file)
-		%ButtonsContainer.add_child(but)
+	if dir != null:
+		dir.list_dir_begin()
+		for file: String in dir.get_files():
+			var but = button.instantiate()
+			but.setup(file)
+			%ButtonsContainer.add_child(but)
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu/main_menu.tscn");
